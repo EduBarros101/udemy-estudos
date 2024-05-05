1cf13933f3decce7d37d02fc2a29b3e86b68fc1b
Eduardo Barros <edu.barros101@gmail.com>
2024-05-05 00:07:35 -0300

    Templates iniciais prontos

---
+++ matheus_battisti/php/15_blog/index.php
@@ -0,0 +1,9 @@
+<?php
+include_once('templates/header.php');
+?>
+
+<h1>Index aqui. Página principal.</h1>
+
+<?php
+include_once('templates/footer.php');
+?>
---
+++ matheus_battisti/php/15_blog/templates/footer.php
@@ -0,0 +1,5 @@
+<footer>Hora de Codar &copy; 2024.</footer>
+
+</body>
+
+</html>
