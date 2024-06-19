0525a7fb9a429f0a4998f96f24988a291c81b9d3
Eduardo Barros <edu.barros101@gmail.com>
2024-06-19 17:16:36 -0300

    Finalizando o CSS

--- matheus_battisti/php/17_agenda/index.php
+++ matheus_battisti/php/17_agenda/index.php
@@ -31,7 +31,7 @@ include_once("templates/header.php");
             <td class="actions">
               <a href="#"><i class="fas fa-eye check-icon"></i></a>
               <a href="#"><i class="fas fa-edit edit-icon"></i></a>
-              <button type="submit"><i class="fas fa-times delete-icon"></i></button>
+              <button class="delete-btn" type="submit"><i class="fas fa-times delete-icon"></i></button>
             </td>
           </tr>
         <?php endforeach ?>
