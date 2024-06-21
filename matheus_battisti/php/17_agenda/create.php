efcdeda3084ca90bccbc85147c532c95f8ca4b2e
Eduardo Barros <edu.barros101@gmail.com>
2024-06-21 14:45:37 -0300

    Tela de adicionar contatos

--- matheus_battisti/php/17_agenda/create.php
+++ matheus_battisti/php/17_agenda/create.php
@@ -4,8 +4,30 @@ include_once("templates/header.php");
 
 ?>
 
-<h1>Criando Contato</h1>
-<i class="fas fa-eye"></i>
+<div class="container">
+  <?php include_once("templates/backbtn.php") ?>
+  <h1 id="main-title">Criar Contato</h1>
+
+  <form id="create-form" action="<?= $BASE_URL ?>config/process.php" method="POST">
+    <input type="hidden" name="type" value="create">
+
+    <div class="form-floating">
+      <input type="text" name="name" id="name" class="form-control" placeholder="Insira o nome" required>
+      <label for="name">Nome do Contato</label>
+    </div>
+
+    <div class="form-floating">
+      <input type="text" name="phone" id="phone" class="form-control" placeholder="Digite o Telefone" required>
+      <label for="phone">Digite o Telefone</label>
+    </div>
+
+    <div class="form-floating">
+      <textarea type="text" name="observations" id="observations" class="form-control" placeholder="Insira as Observações" style="height: 100px;"></textarea>
+      <label for="observations">Insira as Observações</label>
+    </div>
+
+  </form>
+</div>
 
 <?php
 
--- matheus_battisti/php/17_agenda/templates/backbtn.php
+++ matheus_battisti/php/17_agenda/templates/backbtn.php
@@ -1,3 +1,7 @@
-<button id="back-button" type="button" class="btn btn-outline-primary">
-  <a href="<?= $BASE_URL ?>index.php" id="back-link">Voltar</a>
-</button>
+<div id="back-button-container">
+
+
+  <button id="back-button" type="button" class="btn btn-outline-primary">
+    <a href="<?= $BASE_URL ?>index.php" id="back-link">Voltar</a>
+  </button>
+</div>
