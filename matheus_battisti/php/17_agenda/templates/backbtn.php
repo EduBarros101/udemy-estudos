eedf6546811ef7c45d69a42f5ef54e90dc6674f6
Eduardo Barros <edu.barros101@gmail.com>
2024-06-24 11:15:36 -0300

    Ajustes no botão Voltar

--- matheus_battisti/php/17_agenda/templates/backbtn.php
+++ matheus_battisti/php/17_agenda/templates/backbtn.php
@@ -1,6 +1,4 @@
 <div id="back-button-container">
-
-
   <a href="<?= $BASE_URL ?>index.php" id="back-link">
     <button id="back-button" type="button" class="btn btn-outline-primary">
       Voltar
