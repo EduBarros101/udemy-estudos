9b92e6637698b3ac3f452495d2b8b276de9127bf
Eduardo Barros <edu.barros101@gmail.com>
2024-06-25 11:36:53 -0300

    Inserindo dados com DAO

---
+++ matheus_battisti/php/18_design_patterns/1_dao/dao/CarDAO.php
@@ -0,0 +1,31 @@
+<?php
+
+include_once("models/Car.php");
+
+class CarDAO implements CarDAOInterface
+{
+
+  private $conn;
+
+  public function __construct(PDO $conn)
+  {
+    $this->conn = $conn;
+  }
+
+  public function findAll()
+  {
+  }
+
+  public function create(Car $car)
+  {
+
+    $query = "INSERT INTO cars (brand, km, color) VALUES (:brand, :km, :color)";
+    $stmt = $this->conn->prepare($query);
+
+    $stmt->bindParam(":brand", $car->getBrand());
+    $stmt->bindParam(":km", $car->getKm());
+    $stmt->bindParam(":color", $car->getColor());
+
+    $stmt->execute();
+  }
+}
--- matheus_battisti/php/18_design_patterns/1_dao/index.php
+++ matheus_battisti/php/18_design_patterns/1_dao/index.php
@@ -1,3 +1,42 @@
 <?php
 
 include_once("db.php");
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Carros</title>
+</head>
+
+<body>
+  <h1>Insira um carro:</h1>
+
+  <form action="process.php" method="POST">
+
+    <div>
+      <label for="brand">Marca do Carro</label>
+      <input type="text" name="brand" id="brand" placeholder="Insira a marca">
+    </div>
+
+    <div>
+      <label for="km">Kilometragem</label>
+      <input type="text" name="km" id="km" placeholder="Insira a kilometragem">
+    </div>
+
+    <div>
+      <label for="color">Cor</label>
+      <input type="text" name="color" id="color" placeholder="Insira a cor">
+    </div>
+
+    <input type="submit" value="Salvar">
+
+  </form>
+
+</body>
+
+</html>
