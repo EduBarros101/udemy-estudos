4c8dcfa9e1fdaf3f35aa61b033e8dbf41a885900
Eduardo Barros <edu.barros101@gmail.com>
2024-07-03 18:17:12 -0300

    Validação de autenticação

--- matheus_battisti/php/19_moviestar/auth_process.php
+++ matheus_battisti/php/19_moviestar/auth_process.php
@@ -7,6 +7,7 @@ require_once("models/Message.php");
 require_once("dao/UserDAO.php");
 
 $message = new Message($BASE_URL);
+$userDao = new UserDAO($conn, $BASE_URL);
 
 // Resgata o valor de "type" vindo do formulário e armazerna na variável.
 $type = filter_input(INPUT_POST, "type");
@@ -21,6 +22,21 @@ if ($type === "register") {
 
   // verificação de dados mínimos para prosseguir
   if ($email && $name && $lastname && $password) {
+
+    if ($password === $confirmpassword) {
+
+      // verificar se o e-mail já está cadastrado no sistema.
+      if ($userDao->findByEmail($email) === "false") {
+
+        echo "Nenhum usuário encontrado.";
+      } else {
+
+        // Enviar mensagem de erro. Usuário já existe.
+        $message->setMessage("E-mail já cadastrado. Use outro e-mail.", "error", "back");
+      }
+    } else {
+      $message->setMessage("As senhas não são iguais.", "error", "back");
+    }
   } else {
     // Enviar mensagem de erro de dados faltantes.
     $message->setMessage("Preencha todos os campos.", "error", "back");
--- matheus_battisti/php/19_moviestar/dao/UserDAO.php
+++ matheus_battisti/php/19_moviestar/dao/UserDAO.php
@@ -55,6 +55,28 @@ class UserDAO implements UserDAOInterface
 
   public function findByEmail($email)
   {
+
+    if ($email != "") {
+
+      $query = "SELECT * FROM users WHERE email = :email";
+
+      $stmt = $this->conn->prepare($query);
+      $stmt->bindParam(":email", $email);
+
+      $stmt->execute();
+
+      if ($stmt->rowCount() > 0) {
+
+        $data = $stmt->fetch();
+        $user = $this->buildUser($data);
+
+        return $user;
+      } else {
+        return false;
+      }
+    } else {
+      return false;
+    }
   }
 
   public function findById($id)
